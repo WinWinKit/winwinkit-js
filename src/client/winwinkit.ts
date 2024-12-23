a3c89fcb885b6e0d53e553d5c95e9f358be32cab
Oleh <oleh.stasula@gmail.com>
2024-12-23 13:46:08 +0100

    Added referral user methods

--- src/client/winwinkit.ts
+++ src/client/winwinkit.ts
@@ -1,3 +1,7 @@
+import createClient from "openapi-fetch";
+import type {paths} from "../types/schema";
+import {ReferralUser, ReferralUserCreate, ReferralUserUpdate} from "../types/referralUser";
+
 /**
  * WinWinKit Client
  */
@@ -19,5 +23,52 @@ export default class WinWinKit {
     }) {
         this.appUserId = appUserId;
         this.projectKey = projectKey;
+    }
+
+    public async referralUser(): Promise<ReferralUser | undefined> {
+        const client = this.createClient();
+        const {data, error} = await client.GET('/referral/users/{app_user_id}', {
+            path: {app_user_id: this.appUserId},
+            headers: this.createHeaders()
+        });
+        if (error)
+            throw error;
+        return data;
+    }
+
+    public async createReferralUser({referralUser}: {
+        referralUser: ReferralUserCreate
+    }): Promise<ReferralUser | undefined> {
+        const client = this.createClient();
+        const {data, error} = await client.POST('/referral/users', {
+            headers: this.createHeaders(),
+            body: referralUser,
+        });
+        if (error)
+            throw error;
+        return data;
+    }
+
+    public async updateReferralUser({referralUser}: {referralUser: ReferralUserUpdate}): Promise<ReferralUser | undefined> {
+        const client = this.createClient();
+        const {data, error} = await client.PATCH('/referral/users/{app_user_id}', {
+            path: {app_user_id: this.appUserId},
+            headers: this.createHeaders(),
+            body: referralUser,
+        });
+        if (error)
+            throw error;
+        return data;
+    }
+
+    private createClient() {
+        return createClient<paths>({baseUrl: "https://api.winwinkit.com"})
+    }
+
+    private createHeaders() {
+        return {
+            'Content-Type': 'application/json',
+            'X-API-Key': this.projectKey,
+        }
     }
 }
---
+++ src/types/referralUser.ts
@@ -0,0 +1,5 @@
+import {components} from "./schema";
+
+export type ReferralUser = components['schemas']['ReferralUser'];
+export type ReferralUserCreate = components['schemas']['ReferralUserCreate'];
+export type ReferralUserUpdate = components['schemas']['ReferralUserUpdate'];
